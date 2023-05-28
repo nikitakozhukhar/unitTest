46d3f875bb1e89eed0ee0986db12b518bbffa708
NikitaKx9 <nikitakx9@gmail.com>
2023-05-28 21:39:38 +0300

    test v 1

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,24 @@
 import sum from '../basic';
 
+import checkHealth from '../basic'
+
 test('should sum', () => {
   const result = sum([1, 2, 3]);
 
   expect(result).toBe(6);
 });
+
+test('check health', () => {
+  const healthState = checkHealth({name, health});
+
+  expect(healthState).toBe('healthy')
+})
+
+
+// export default function sum(items) {
+//   let result = 0;
+//   for (const item of items) {
+//     result += item;
+//   }
+//   return result;
+// }
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,7 +1,26 @@
-export default function sum(items) {
-  let result = 0;
-  for (const item of items) {
-    result += item;
+// export default function sum(items) {
+//   let result = 0;
+//   for (const item of items) {
+//     result += item;
+//   }
+//   return result;
+// }
+
+export function checkHealth({name, health}) {
+  if (health >= 51) {
+    console.log('healthy')
   }
-  return result;
+  if (health >= 15 && health <= 50) {
+    console.log('wounded')
+  }
+  else {
+    console.log('critical')
+  }
+  return 
 }
+
+let character = {
+  name: 'маг',
+  health: 90,
+}
+checkHealth(character);
